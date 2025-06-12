ad99ca57eb90bf7bcf0cc1c960db748b4154d3dd
Risqie <risqienurilman@gmail.com>
2025-06-13 07:42:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,41 @@
-import sys
-import os
-from utils.extract import extract_products  # Memanggil extract_products
-from utils.transform import transform_products
-from utils.load import load_to_csv, load_to_gsheet, load_to_postgres
-import datetime
+from utils.extract import scrape_fashion
+from utils.transform import transform_to_DataFrame, transform_data
+from utils.load import store_to_postgre, store_to_csv, store_to_sheets
 
-# Menambahkan utils ke dalam path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'utils')))
 
 def main():
-    # Ekstraksi data dari website
-    print("Extracting data...")
-    raw_data = extract_products()  # Panggil extract_products untuk mengekstrak data
+    BASE_URL = 'https://fashion-studio.dicoding.dev/page{}'
+    EXCHANGE_RATE = 16000
+    DB_URL = 'postgresql+psycopg2://postgres:postgres@localhost:5432/fashiondb'
 
-    print("Scraping page {page}...") # Debugging proses ekstrak
+    print("Memulai proses scraping...")
+    all_fashion_data = scrape_fashion(BASE_URL)
 
-    print("Raw Data Extracted:", raw_data)  # Debugging: Cek data yang diekstrak
-    
-    # Periksa ekstraksi data
-    print("Extracted Raw Data:", raw_data)
+    if not all_fashion_data:
+        print("❌ Tidak ada data yang ditemukan.")
+        return
 
-    # Menambah timestamp saat data diekstraksi
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    for item in raw_data:
-        item['timestamp'] = timestamp
+    try:
+        print("Transformasi data...")
+        df = transform_to_DataFrame(all_fashion_data)
+        df = transform_data(df, EXCHANGE_RATE)
 
-    # Transformasi data
-    print("Transforming data...")
-    transformed_data = transform_products(raw_data)
+        print("Menyimpan ke CSV...")
+        store_to_csv(df)
 
-    # Menyimpan data ke dalam CSV
-    print("Loading data to CSV...")
-    load_to_csv(transformed_data)
+        print("Menyimpan ke PostgreSQL...")
+        store_to_postgre(df, DB_URL)
 
-    # Menyimpan data ke Google Sheets
-    print("Loading data to Google Sheets...")
-    load_to_gsheet(transformed_data)
+        print("Menyimpan ke Google Sheets...")
+        store_to_sheets(df)
 
-    # Menyimpan data ke PostgreSQL
-    print("Loading data to PostgreSQL...")
-    load_to_postgres(transformed_data)
+        print("\n✅ Pipeline ETL selesai!\n")
+        print('-' * 40)
+        print(df.info())
 
-    print("ETL process completed.")
+    except Exception as e:
+        print(f"❌ Terjadi kesalahan dalam proses ETL: {e}")
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
